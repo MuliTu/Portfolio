3bc6449c244f3174ab632184782ba7d9ce9c7050
MuliTu <muli.turiel@gmail.com>
2019-04-01 22:42:36 +0300

    Updates

---
+++ precache-manifest.10d690f401646f35392774f0dec0ea5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "c9a9581cb50748249d87",
+    "url": "/Portfolio/static/js/main.3ef943a8.chunk.js"
+  },
+  {
+    "revision": "c5b99c286acdac743614",
+    "url": "/Portfolio/static/js/2.1c138b67.chunk.js"
+  },
+  {
+    "revision": "c9a9581cb50748249d87",
+    "url": "/Portfolio/static/css/main.8324ec7f.chunk.css"
+  },
+  {
+    "revision": "ec98cee06f920ba1bb49a7df56b8edd0",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.a363be563fb57eb5de7108a23f141d5e.js"
+  "/Portfolio/precache-manifest.10d690f401646f35392774f0dec0ea5b.js"
 );
 
 workbox.clientsClaim();
