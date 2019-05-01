74925bfee7e793cd24f2326f976cafdcd47e4767
MuliTu <muli.turiel@gmail.com>
2019-05-01 16:00:02 +0300

    added mobx to skills

--- src/data/skills/stateManagement.js
+++ src/data/skills/stateManagement.js
@@ -1,17 +1,22 @@
 export const stateManagement = {
-    category: 'State Management',
-    list: [
-        {
-            name: 'Vuex',
-            img: 'https://user-images.githubusercontent.com/7110136/29002857-9e802f08-7ab4-11e7-9c31-604b5d0d0c19.png'
-        },
-        {
-            name: 'Redux',
-            img: 'http://www.stickpng.com/assets/images/5848309bcef1014c0b5e4a9a.png'
-        },
-        {
-            name: 'React-Hooks',
-            img:'http://pngimg.com/uploads/fish_hook/fish_hook_PNG2.png'
-        }
-    ],
-}
+  category: "State Management",
+  list: [
+    {
+      name: "Vuex",
+      img:
+        "https://user-images.githubusercontent.com/7110136/29002857-9e802f08-7ab4-11e7-9c31-604b5d0d0c19.png"
+    },
+    {
+      name: "Redux",
+      img: "http://www.stickpng.com/assets/images/5848309bcef1014c0b5e4a9a.png"
+    },
+    {
+      name: "React-Hooks",
+      img: "http://pngimg.com/uploads/fish_hook/fish_hook_PNG2.png"
+    },
+    {
+      name: "Mobx",
+      img: "https://mobx.js.org/docs/mobx.png"
+    }
+  ]
+};
